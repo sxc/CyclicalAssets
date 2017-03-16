a59297c70bd57dbde115fe57e22387014b7c0828
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-16 09:04:59 +0800

    Initial Commit

---
+++ CyclicalAssets/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CyclicalAssets
+//
+//  Created by Shen Xiaochun on 2017/3/16.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
