89247cbf48581afe6d9e0a64c7c05b1de66588bc
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-16 14:28:06 +0800

    add Closures

---
+++ CyclicalAssets/Accountant.swift
@@ -0,0 +1,24 @@
+//
+//  Accountant.swift
+//  CyclicalAssets
+//
+//  Created by Shen Xiaochun on 2017/3/16.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import Foundation
+
+class Accountant {
+    typealias NetWorthChanged = (Double) -> Void
+    
+    var netWorthChangedHandler: NetWorthChanged? = nil
+    var netWorth: Double = 0.0 {
+        didSet {
+            netWorthChangedHandler?(netWorth)
+        }
+    }
+    
+    func gained(_ asset: Asset) {
+        netWorth += asset.value
+    }
+}
