34b728a22abf2d3255e3d519836a14a71c258d48
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-16 14:27:32 +0800

    add ARC in Closures

---
+++ CyclicalAssets/Person.swift
@@ -0,0 +1,46 @@
+//
+//  Person.swift
+//  CyclicalAssets
+//
+//  Created by Shen Xiaochun on 2017/3/16.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import Foundation
+
+class Person: CustomStringConvertible {
+    let name: String
+    let accountant = Accountant()
+    var assets = [Asset]()
+    
+    var description: String {
+        return "Person(\(name))"
+    }
+    
+    init(name: String) {
+        self.name = name
+        
+        accountant.netWorthChangedHandler = {
+            [weak self] netWorth in
+            self?.netWorthDidChange(to: netWorth)
+            return
+        }
+        
+    }
+    
+    deinit {
+        print("\(self) is being deallocated")
+    }
+    
+    func takeOwnership(of asset: Asset) {
+        asset.owner = self
+        assets.append(asset)
+        
+        accountant.gained(asset)
+    }
+    
+    func netWorthDidChange(to netWorth: Double) {
+        print("The net worth of \(self) is now \(netWorth)")
+    }
+    
+}
--- CyclicalAssets/main.swift
+++ CyclicalAssets/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+var bob: Person? = Person(name: "Bob")
+print("created \(bob)")
+
+
+var laptop: Asset? = Asset(name: "Shiny Laptop", value: 1_500.0)
+var hat: Asset? = Asset(name: "Cowboy Hat", value: 175.0)
+var backpack: Asset? = Asset(name: "Blue Backpack", value: 45.0)
+
+bob?.takeOwnership(of: laptop!)
+bob?.takeOwnership(of: hat!)
+
+print("While Bob is alive, hat's owner is \(hat!.owner)")
+
+
+
+
+bob = nil
+print("the bob variable is now \(bob)")
+print("After Bob is deallocated, hat's owner is \(hat!.owner)")
+
+
+laptop = nil
+hat = nil
+backpack = nil
+
+
+
 
